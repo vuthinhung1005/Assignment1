565b43fd6edd2b82f5567ad1dcb2bee8f7d94e2d
vuthinhung1005 <nhungvt19868@fpt.edu.vn>
2023-05-27 14:47:42 +0700

    ass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
+import { HomeComponent } from './home/home/home.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: BaseLayoutComponent, children: [
+      { path: "", component: HomeComponent },
+      { path: "about", component: AboutPageComponent },
+      { path: "product/:id", component: ProductDetailComponent }
+    ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent },
+      { path: "product", component: ProductListComponent },
+      { path: "product/add", component: ProductAddComponent },
+      { path: "product/:id/edit", component: ProductEditComponent }
+    ]
+  },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
+import { HomeComponent } from './home/home/home.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { ProdcutAgeComponent } from './pages/prodcut-age/prodcut-age.component';
 
+// decorators
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    HomeComponent,
+    BaseLayoutComponent,
+    AdminLayoutComponent,
+    AboutPageComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    DashboardComponent,
+    AdminProductComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductDetailComponent,
+    AdminComponent,
+    ProdcutAgeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
